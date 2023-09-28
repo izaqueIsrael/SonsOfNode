49c8f1b33130b8bd7807dac2850428fffe8c003f
Isaac <isaac@pop-os.localdomain>
2023-09-28 13:51:40 -0300

    adding footer to routes

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,21 @@ import Image from 'next/image';
 import Popup from '@/components/Popup';
 import Navbar from '@/components/Navbar';
 import error from '@/images/erro.gif';
+import Footer from '@/components/Footer';
 
 const erro = ({ popupIsOpen, handlePopupIsOpen }) => {
   return (
-    <section className="error">
-      <Navbar handlePopupIsOpen={handlePopupIsOpen} />
-      <Popup popupIsOpen={popupIsOpen} handlePopupIsOpen={handlePopupIsOpen} />
-      <Image src={error} alt="erro 404" style={{ margin: '25%' }} />
-    </section>
+    <>
+      <section className="error">
+        <Navbar handlePopupIsOpen={handlePopupIsOpen} />
+        <Popup
+          popupIsOpen={popupIsOpen}
+          handlePopupIsOpen={handlePopupIsOpen}
+        />
+        <Image src={error} alt="erro 404" style={{ margin: '25%' }} />
+      </section>
+      <Footer />
+    </>
   );
 };
 
--- src/pages/garithos.js
+++ src/pages/garithos.js
@@ -2,24 +2,31 @@ import Image from 'next/image';
 import Popup from '@/components/Popup';
 import Navbar from '@/components/Navbar';
 import garithos from '@/images/garithos.webp';
+import Footer from '@/components/Footer';
 
 const GarithosTribute = ({ popupIsOpen, handlePopupIsOpen }) => {
   return (
-    <section className="garithos">
-      <Navbar handlePopupIsOpen={handlePopupIsOpen} />
-      <Popup popupIsOpen={popupIsOpen} handlePopupIsOpen={handlePopupIsOpen} />
-      <div className="garithos__container">
-        <h1 className="title article__title">
-          Homenagem ao Grande Marshal Othmar Garithos
-        </h1>
-        <p className="text test__text">
-          Othmar Garithos foi um bravo líder humano, cujo legado vive até hoje.
-          Embora suas decisões possam ter sido questionadas, seu valor e
-          compromisso com seu povo nunca podem ser negados.
-        </p>
-        <Image src={garithos} alt="The Grand Marshal Othmar Garithos" />
-      </div>
-    </section>
+    <>
+      <section className="garithos">
+        <Navbar handlePopupIsOpen={handlePopupIsOpen} />
+        <Popup
+          popupIsOpen={popupIsOpen}
+          handlePopupIsOpen={handlePopupIsOpen}
+        />
+        <div className="garithos__container">
+          <h1 className="title article__title">
+            Homenagem ao Grande Marshal Othmar Garithos
+          </h1>
+          <p className="text test__text">
+            Othmar Garithos foi um bravo líder humano, cujo legado vive até
+            hoje. Embora suas decisões possam ter sido questionadas, seu valor e
+            compromisso com seu povo nunca podem ser negados.
+          </p>
+          <Image src={garithos} alt="The Grand Marshal Othmar Garithos" />
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 };
 
